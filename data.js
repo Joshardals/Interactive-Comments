1d59fbb42cac66297bc3c68638f5470974bda26f
Joshardals <joshuabamidele219@gmail.com>
2022-03-31 17:52:18 +0100

    initial commit

--- data.js
+++ data.js
@@ -30,7 +30,7 @@ export const data = [
     content:
       "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
     createdAt: "2 weeks ago",
-    score: 5,
+    score: 4,
     user: {
       image: {
         png: "/avatars/image-maxblagun.png",
