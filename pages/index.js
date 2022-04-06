df229cc66bc0fc3682b630771fa9f78f36decfed
Joshardals <joshuabamidele219@gmail.com>
2022-04-06 19:29:32 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import Head from "next/head";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import tw from "tailwind-styled-components";
 import Body from "../components/Body";
 
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.push("/login");
+    }
+  }, []);
   return (
     <Wrapper>
       <Head>
@@ -17,8 +28,10 @@ export default function Home() {
 const Wrapper = tw.div``;
 
 export async function getServerSideProps(ctx) {
+  const providers = await getProviders();
   return {
     props: {
+      providers,
       session: await getSession(ctx),
     },
   };
